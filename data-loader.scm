3b8a865cc2ba30a887a826eb71b46817fb8fdf8f
Gurtej Kanwar <gurtej@mit.edu>
2014-05-09 01:49:40 -0400

    Changes map to be a multi-map using fluid-let for better syntax

--- data-loader.scm
+++ data-loader.scm
@@ -25,18 +25,47 @@
    (conspire:null-job))
  (with-time-sharing-conspiracy test1)
 
- (define (test1)
+ (define (test2)
    (define ds-input (create-mrq-data-set))
    (define ds1 (create-mrq-data-set))
    (mrs:map
     (lambda (key value)
       (pp value)
-      (create-ds-elt key (* 10 value)))
+      (mrs:emit key (* 10 value)))
     ds-input
     ds1)
    (mrs:print-streaming ds1)
    (mrs:feed-value-list ds-input '(1 2 3 4))
    (conspire:null-job))
- (with-time-sharing-conspiracy test1)
+ (with-time-sharing-conspiracy test2)
+;-> (0 10)
+;   (1 20)
+;   (2 30)
+;   (3 40)
 
+(define (test3)
+   (define ds-input (create-mrq-data-set))
+   (define ds1 (create-mrq-data-set))
+   (mrs:map
+    (lambda (key value)
+      (mrs:emit key (* 10 value)))
+    ds-input
+    ds1)
+   (mrs:map
+    (lambda (key value)
+      (mrs:emit key (* 11 value)))
+    ds-input
+    ds1)
+   (mrs:print-streaming ds1)
+   (mrs:feed-value-list ds-input '(1 2 3 4))
+   (conspire:null-job))
+ (with-time-sharing-conspiracy test3)
+;-> (0 10)
+;   (1 20)
+;   (2 30)
+;   (3 40)
+;   (0 11)
+;   (1 22)
+;   (2 33)
+;   (3 44)
 |#
--- operations.scm
+++ operations.scm
@@ -1,8 +1,29 @@
-(define (mrs:map map-func ds-in ds-out)
+;;; Kind of gross, but we need to define this somewhere for the fluid-let
+;;; operations below
+(define (mrs:emit key value) (error "mrs:emit undefined for this operation"))
+
+;;; Map (actually a multi-map)
+;;; Requires a function from key,value which calls emit on key.value
+;;; pairs to output.
+(define (mrs:map multi-map-func ds-in ds-out)
+  (define (mm-func kv-emit)
+      (lambda (ds-elt)
+        (fluid-let ((mrs:emit
+                     (lambda (key value)
+                       (kv-emit (create-ds-elt key value)))))
+          (multi-map-func (ds-elt-key ds-elt) (ds-elt-value ds-elt)))))
+  (make-distributor mm-func ds-in ds-out 5))
+
+;;; Filter
+;;; Requires a function from key,value to #t/#f
+(define (mrs:filter filter-func ds-in ds-out)
   (define (mm-func emit)
     (lambda (ds-elt)
-      (emit (map-func (ds-elt-key ds-elt) (ds-elt-value ds-elt)))))
+      (if (filter-func (ds-elt-key ds-elt) (ds-elt-value ds-elt))
+          (emit ds-elt))))
   (make-distributor mm-func ds-in ds-out 5))
+
+;;; 
 
 (define (mrs:print-streaming ds-in)
   (define (mm-func emit)

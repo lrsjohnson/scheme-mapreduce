35df683bb70e83951e71d8ef19874250102b27a6
Gurtej Kanwar <gurtej@mit.edu>
2014-05-08 23:55:22 -0400

    Fixes a few bugs, adds test to Mr. Queue

--- multi-reader-queue.scm
+++ multi-reader-queue.scm
@@ -5,15 +5,15 @@
 (define (make-mr-queue-end) (list *mr-queue-end*))
 (define (mr-queue-end-val? val) (eq? *mr-queue-end* val))
 
-(define (make-mr-queue)
+(define (create-mr-queue)
   (let ((element-list (make-mr-queue-end)))
-    (list 'mr-queue element-list element-list 0 '())))
+    (make-mr-queue element-list element-list 0 '())))
 
 (define (mr-queue-add-elt mr-queue elt)
   (let ((end-ptr (mr-queue-end-ptr mr-queue)))
     (set-car! end-ptr elt)
     (set-cdr! end-ptr (make-mr-queue-end))
-    (set-mr-queue-end-ptr! (cdr end-ptr))))
+    (set-mr-queue-end-ptr! mr-queue (cdr end-ptr))))
 
 (define (mr-queue-garbage-collect mr-queue)
   (let ((min-reader-index 
@@ -22,8 +22,8 @@
                        (reader-info-index reader-info))
                      (mr-queue-reader-infos mr-queue)))))
     (cond ((= (- min-reader-index 1) (mr-queue-deleted-count mr-queue))
-           (set-mr-queue-deleted-count! min-reader-index)
-           (set-mr-queue-element-list! (cdr (mr-queue-element-list mr-queue))))
+           (set-mr-queue-deleted-count! mr-queue min-reader-index)
+           (set-mr-queue-element-list! mr-queue (cdr (mr-queue-element-list mr-queue))))
           ((= min-reader-index (mr-queue-deleted-count mr-queue))
            'continue)
           (else
@@ -32,15 +32,50 @@
 
 (define (mr-queue-get-reader mr-queue)
   (let ((reader-info (make-reader-info (mr-queue-element-list mr-queue)
-                                       (mr-queue-deleted-count mr-queue))))
-    (set! reader-infos (cons reader-info reader-infos))
+                                       (mr-queue-deleted-count mr-queue)))
+        (reader-infos (mr-queue-reader-infos mr-queue)))
+    (set-mr-queue-reader-infos! mr-queue (cons reader-info reader-infos))
     (lambda ()
       (let ((ptr (reader-info-ptr reader-info))
-            ((index (reader-info-index reader-info))))
+            (index (reader-info-index reader-info)))
         (let ((val (car ptr)))
           (if (not (mr-queue-end-val? val))
               (begin
-                (set-reader-info-ptr! (cdr ptr))
-                (set-reader-index! (+ 1 index))
+                (set-reader-info-ptr! reader-info (cdr ptr))
+                (set-reader-info-index! reader-info (+ 1 index))
                 (mr-queue-garbage-collect mr-queue)))
           val)))))
+
+#|
+(define queue (create-mr-queue))
+(mr-queue-add-elt queue 1)
+(define reader (mr-queue-get-reader queue))
+(reader)
+;-> 1
+(reader)
+;-> (mr-queue-end)
+(reader)
+;-> (mr-queue-end)
+(mr-queue-add-elt queue 2)
+(reader)
+;-> 2
+(reader)
+;-> (mr-queue-end)
+(define reader2 (mr-queue-get-reader queue))
+(reader2)
+;-> (mr-queue-end)
+(mr-queue-add-elt queue 3)
+(mr-queue-add-elt queue 4)
+(reader)
+;-> 3
+(reader2)
+;-> 3
+(reader2)
+;-> 4
+(reader)
+;-> 4
+(mr-queue-add-elt queue 5)
+(define reader3 (mr-queue-get-reader queue))
+(reader3)
+;-> 5
+|#
